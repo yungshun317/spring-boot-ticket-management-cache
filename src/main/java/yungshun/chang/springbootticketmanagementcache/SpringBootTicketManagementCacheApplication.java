67a08f87b9cbdcf8c3a3018b7737686596bd618a
yungshun317 <seraph317@gmail.com>
2021-06-14 21:01:10 +0800

    [CHORE] Initialize a Spring Boot project

---
+++ src/main/java/yungshun/chang/springbootticketmanagementcache/SpringBootTicketManagementCacheApplication.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springbootticketmanagementcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTicketManagementCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTicketManagementCacheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yungshun/chang/springbootticketmanagementcache/SpringBootTicketManagementCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package yungshun.chang.springbootticketmanagementcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTicketManagementCacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
