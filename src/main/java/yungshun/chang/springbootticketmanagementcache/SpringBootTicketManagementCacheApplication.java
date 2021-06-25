eedc5dd3ba07a4545ebd60eb1a89752bd55fa535
yungshun317 <seraph317@gmail.com>
2021-06-25 14:45:01 +0800

    [FEAT] Cache with ETags using Spring's `ShallowEtagHeaderFilter`

--- src/main/java/yungshun/chang/springbootticketmanagementcache/SpringBootTicketManagementCacheApplication.java
+++ src/main/java/yungshun/chang/springbootticketmanagementcache/SpringBootTicketManagementCacheApplication.java
@@ -2,6 +2,11 @@ package yungshun.chang.springbootticketmanagementcache;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
 
 @SpringBootApplication
 public class SpringBootTicketManagementCacheApplication {
@@ -10,4 +15,18 @@ public class SpringBootTicketManagementCacheApplication {
         SpringApplication.run(SpringBootTicketManagementCacheApplication.class, args);
     }
 
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+
+    @Bean
+    public FilterRegistrationBean shallowEtagHeaderFilterRegistration() {
+        FilterRegistrationBean result = new FilterRegistrationBean();
+        result.setFilter(this.shallowEtagHeaderFilter());
+        result.addUrlPatterns("/user/*");
+        result.setName("shallowEtagHeaderFilter");
+        result.setOrder(1);
+        return result;
+    }
 }
